7be2146027b4ee9aa962589b9df3f5cf1f4243e3
Minh Phí <phitaiminh@gmail.com>
2026-05-10 20:53:20 +0700

    Revert "Merge pull request #3 from TaiMinh2002/develop"
    This reverts commit 62bd341dcfb588594a995c32603a97ece614f595, reversing
    changes made to bef0e50755c1d5eaf1d13fac8b28c8371383908c.
    
    git push origin main#	deleted:    lib/features/reports/presentation/pages/report_detail_screen.dart

--- lib/core/localization/localization_extensions.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/widgets.dart';
-import 'package:pet_finder/l10n/app_localizations.dart';
-
-extension LocalizationExtensions on BuildContext {
-  AppLocalizations get l10n => AppLocalizations.of(this);
-}
